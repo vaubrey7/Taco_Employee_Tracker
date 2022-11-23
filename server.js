4ba957970fdff6f4cef9e2f40ac8dc1d68896d6e
Vaughn Aubrey <vaubrey7@gmail.com>
2022-11-22 18:02:42 -0700

    final

--- server.js
+++ server.js
@@ -208,7 +208,8 @@ const addEmployee = () => {
               `INSERT INTO employees(first_name, last_name, role_id, manager_id) VALUES(?, ?, 
               (SELECT id FROM roles WHERE title = ? ), 
               (SELECT id FROM (SELECT id FROM employees WHERE CONCAT(first_name," ",last_name) = ? ) AS tmptable))`, [answer.fName, answer.lName, answer.role, answer.manager]
-          )
+         
+              )
           startApp();
       })
   })
